3b35e3650d9a223896ad1e178cadb3c69d645c5b
warkesiddharth <warkeofficial@gmail.com>
2023-02-05 22:04:54 +0530

    final

--- js1.js
+++ js1.js
@@ -5,74 +5,98 @@ function getcomputerchoice() {
     return arr[Math.floor(Math.random() * arr.length)];
 
 }
-function playervalue() {
-    let value = window.prompt("your input for the game ?");
-    return value.toUpperCase();
-}
 
+let playerscore = 0;
+let computerscore = 0;
+let result;
+let win = false;
+
+const div = document.createElement('div');
+const initial = document.querySelector('#initial');
+initial.after(div);
+const div2 = document.createElement('div');
+div.after(div2);
+div.textContent = " ";
+div2.textContent = " ";
 
 function playround(playerselection, computerselction) {
 
+
+
     if (playerselection == "ROCK" && computerselction == "SCISSOR") {
-        return "You Win! Rock beats Scissor";
+        result = "You Win! Rock beats Scissor";
+        playerscore++;
     } else if (playerselection == "ROCK" && computerselction == "PAPER") {
-        return "You Lose! Paper beats Rock";
+        result = "You Lose! Paper beats Rock";
+        computerscore++;
     } else if (playerselection == "ROCK" && computerselction == "ROCK") {
-        return "Match Drawn!";
+        result = "Match Drawn!";
     } else if (playerselection == "PAPER" && computerselction == "SCISSOR") {
-        return "You Lose! Scissor beats paper";
+        result = "You Lose! Scissor beats paper";
+        computerscore++;
     } else if (playerselection == "PAPER" && computerselction == "ROCK") {
-        return "You Win! PAPER beats ROCK";
+        result = "You Win! PAPER beats ROCK";
+        playerscore++;
     } else if (playerselection == "PAPER" && computerselction == "PAPER") {
-        return "Match Drawn";
+        result = "Match Drawn";
     } else if (playerselection == "SCISSOR" && computerselction == "SCISSOR") {
-        return "MATCH DRAWN";
+        result = "MATCH DRAWN";
     } else if (playerselection == "SCISSOR" && computerselction == "ROCK") {
-        return "You LOSE! ROCK beats Scissor";
+        result = "You LOSE! ROCK beats Scissor";
+        computerscore++;
     } else {
-        return "You Win! Scissor beats Paper";
+        result = "You Win! Scissor beats Paper";
+        playerscore++;
     }
-}
 
-function game(playround) {
-    let playerscore = 0;
-    let computerscore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerselection = playervalue();
-        let computerselction = getcomputerchoice();
-        let result = playround(playerselection, computerselction);
-        let diff = Math.abs(playerscore - computerscore);
-        if (diff == 3) {
-            if (playerscore > computerscore) {
-                return "WINNER!!";
-            }
-            else {
-                return "LOSSER!!";
-            }
-        }
 
-        if (result[4] == 'W') {
-            playerscore++;
-            console.log(result);
-        }
-        else {
-            computerscore++;
-            console.log(result);
-        }
+
+
+    console.log(result);
+    div.textContent = `The resul of the played round is ${result}.\nThe current player-score and computer-score are ${computerscore} and ${playerscore} `;
+
+    if (playerscore == 5) {
+        div2.textContent = "WINNER";
+        win = true;
 
     }
-    if (playerscore > computerscore) {
-        return "WINNER!!";
+    else if (computerscore == 5) {
+        div2.textContent = "A FUCKING LOSSER U R!";
+        win = true;
+
     }
-    else {
-        return "LOSSER!!";
-    }
+
+
+
+
 
 }
 
 
 
+var rock = document.querySelector('#rock');
+var paper = document.querySelector('#paper');
+var scissor = document.querySelector('#scissor');
 
 
 
-console.log(game(playround));
+rock.addEventListener('click', function (e) {
+    playround("ROCK", getcomputerchoice());
+});
+paper.addEventListener('click', function (e) {
+    playround("PAPER", getcomputerchoice());
+});
+scissor.addEventListener('click', function (e) {
+    playround("SCISSOR", getcomputerchoice());
+});
+
+
+
+
+
+
+
+
+
+
+
